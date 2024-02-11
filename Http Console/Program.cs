6cbfd616a42b6fc4f7279c0da824fb9df55710cc
DamianCannon <damianbcannon@hotmail.com>
2024-02-11 17:28:24 +0000

    Refactor how json stream is handled

--- Http Console/Program.cs
+++ Http Console/Program.cs
@@ -1,26 +1,30 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
 using WebAPIClient;
-using HttpClient client = new();
-
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-await ProcessRepositoriesAsync(client);
-
-static async Task ProcessRepositoriesAsync(HttpClient client)
+internal class Program
 {
-    using (var stream = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos"))
+    private static async Task Main(string[] args)
     {
-        var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
-
-        foreach (var repo in repositories ?? Enumerable.Empty<Repository>())
+        using (HttpClient client = new())
         {
-            Console.WriteLine($"{repo.Name}: {repo.FullName}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var repositories = await ProcessRepositoriesAsync(client);
+            foreach (var repo in repositories ?? Enumerable.Empty<Repository>())
+            {
+                Console.WriteLine($"{repo.Name}: {repo.FullName}");
+            }
         }
     }
 
-    //var json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
-    //Console.Write(json.ToString());
+    static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+    {
+        using (var stream = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos"))
+        {
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+            return repositories ?? [];
+        }
+    }
 }
